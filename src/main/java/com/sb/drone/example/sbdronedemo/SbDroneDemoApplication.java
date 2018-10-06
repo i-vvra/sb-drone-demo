79ff8d02065aa2b0e0e481b279033eab3df09df4
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-10-07 03:50:56 +0530

    First cut

---
+++ src/main/java/com/sb/drone/example/sbdronedemo/SbDroneDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sb.drone.example.sbdronedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDroneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDroneDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sb/drone/example/sbdronedemo/SbDroneDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sb.drone.example.sbdronedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbDroneDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
