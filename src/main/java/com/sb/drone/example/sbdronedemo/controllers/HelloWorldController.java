b20c6590f9c28d59b202da82eabdc4f798a2c258
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-10-19 07:53:12 +0530

    Add endpoint, add svc

--- src/main/java/com/sb/drone/example/sbdronedemo/controllers/HelloWorldController.java
+++ src/main/java/com/sb/drone/example/sbdronedemo/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.sb.drone.example.sbdronedemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return "Hello "+name;
+    }
 
 }
