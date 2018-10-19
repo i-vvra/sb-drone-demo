b0bb067cc2f988e1ff8f8729dd102d095d83b8ec
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-10-19 08:58:34 +0530

    Add employee endpoint

---
+++ src/main/java/com/sb/drone/example/sbdronedemo/models/Employee.java
@@ -0,0 +1,10 @@
+package com.sb.drone.example.sbdronedemo.models;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String name;
+    private String gender;
+    private int id;
+}
