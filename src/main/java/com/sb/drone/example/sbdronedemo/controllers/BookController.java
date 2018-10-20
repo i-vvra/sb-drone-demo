481d0124edf7d31390ce547c2d170580d93dcd3e
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-10-20 13:38:45 +0530

    Add book create endpoint

--- src/main/java/com/sb/drone/example/sbdronedemo/controllers/BookController.java
+++ src/main/java/com/sb/drone/example/sbdronedemo/controllers/BookController.java
@@ -2,25 +2,33 @@ package com.sb.drone.example.sbdronedemo.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.sb.drone.example.sbdronedemo.models.Book;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
 
     private Map<Integer, Book> books = new HashMap<>();
+    private AtomicInteger sequence = new AtomicInteger();
 
     public BookController() {
-
     }
 
     @GetMapping("/books/{id}")
     public Book findBookById(@PathVariable int id) {
         return books.get(id);
+    }
+
+    @PostMapping("/books")
+    public void newBook(@RequestBody Book book) {
+        books.put(sequence.incrementAndGet(), book);
     }
 
 }
--- src/main/java/com/sb/drone/example/sbdronedemo/models/Book.java
+++ src/main/java/com/sb/drone/example/sbdronedemo/models/Book.java
@@ -7,5 +7,4 @@ public class Book {
     private int id;
     private String name;
     private String isbn;
-    private Author author;
 }
